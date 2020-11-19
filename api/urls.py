44365f179f5419b3b82dadb10320a1940aeeff6c
shahriar100 <shahriar.hasan100@gmail.com>
2020-11-19 23:59:58 +0600

    Added frontend app

--- api/urls.py
+++ api/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.api, name='api'),
+    # path('', views.api, name='api'),
 
-    path('api', views.apiOverview, name='api-overview'),
+    path('', views.apiOverview, name='api-overview'),
     path('task-list/', views.taskList, name='task-list'),
     path('task-detail/<str:pk>/', views.taskDetail, name='task-detail'),
     path('task-create/', views.taskCreate, name='task-create'),
