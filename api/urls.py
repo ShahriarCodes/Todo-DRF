148ba0ceb38c6a981c8b52295ede1f44be0bb6c0
shahriar100 <shahriar.hasan100@gmail.com>
2020-11-19 13:18:24 +0600

    basic get request api

---
+++ api/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('api', views.apiOverview, name='api-overview'),
+    path('task-list/', views.taskList, name='task-list'),
+    path('task-detail/<str:pk>/', views.taskDetail, name='task-detail'),
+
+    path('', views.api, name='api'),
+]
