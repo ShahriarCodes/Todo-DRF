571afc8113095c21c464970b24b82cb9f539724d
shahriar100 <shahriar.hasan100@gmail.com>
2020-11-19 10:27:52 +0600

    Started project -initial commit

---
+++ serializers_and_crud/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for serializers_and_crud project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'serializers_and_crud.settings')
+
+application = get_asgi_application()
